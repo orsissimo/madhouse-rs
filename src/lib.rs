f74f2fbabab7104a859712dfe9f47ba020083037
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2025-02-05 17:48:50 -0500

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,21 @@
 pub struct ExampleState {
-  value: i32,
+    value: i32,
 }
 
 impl ExampleState {
-  pub fn new() -> Self {
-      Self { value: 0 }
-  }
+    pub fn new() -> Self {
+        Self { value: 0 }
+    }
 
-  pub fn increment(&mut self) {
-      self.value += 1;
-  }
+    pub fn increment(&mut self) {
+        self.value += 1;
+    }
 
-  pub fn decrement(&mut self) {
-      self.value -= 1;
-  }
+    pub fn decrement(&mut self) {
+        self.value -= 1;
+    }
 
-  pub fn get_value(&self) -> i32 {
-      self.value
-  }
+    pub fn get_value(&self) -> i32 {
+        self.value
+    }
 }
