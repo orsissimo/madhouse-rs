72ce24a2faca5ca588dbe7b53060913c9ebdbf4d
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2025-02-05 23:59:50 +0100

    Housekeeping

--- tests/command_test.rs
+++ tests/command_test.rs
@@ -1,5 +1,60 @@
 use proptest::prelude::*;
-use rust_command_testing::{Command, DecrementCommand, ExampleState, IncrementCommand};
+
+pub struct ExampleState {
+  value: i32,
+}
+
+impl ExampleState {
+  pub fn new() -> Self {
+      Self { value: 0 }
+  }
+
+  pub fn increment(&mut self) {
+      self.value += 1;
+  }
+
+  pub fn decrement(&mut self) {
+      self.value -= 1;
+  }
+
+  pub fn get_value(&self) -> i32 {
+      self.value
+  }
+}
+
+/// A trait that all commands must implement.
+pub trait Command {
+    fn check(&self, state: &ExampleState) -> bool;
+    fn apply(&self, state: &mut ExampleState);
+}
+
+/// Increment command.
+#[derive(Debug, Clone)]
+pub struct IncrementCommand;
+
+impl Command for IncrementCommand {
+    fn check(&self, _state: &ExampleState) -> bool {
+        true // Always allowed.
+    }
+
+    fn apply(&self, state: &mut ExampleState) {
+        state.increment();
+    }
+}
+
+/// Decrement command.
+#[derive(Debug, Clone)]
+pub struct DecrementCommand;
+
+impl Command for DecrementCommand {
+    fn check(&self, state: &ExampleState) -> bool {
+        state.get_value() > 0 // Prevents negative values.
+    }
+
+    fn apply(&self, state: &mut ExampleState) {
+        state.decrement();
+    }
+}
 
 proptest! {
     #[test]
