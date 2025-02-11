e877e8b772f643a7fa32d2510151a1a4f1de0238
BowTiedRadone <bowtiedradone@gmail.com>
2025-02-11 15:49:02 +0200

    Add command for submitting block commits

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,8 @@ fn main() {
 pub struct ExampleState {
     value: i32,
     running_miners: Vec<Vec<u8>>,
+    last_mined_block: u64,
+    block_commits: HashMap<u64, Vec<Vec<u8>>>,
 }
 
 impl ExampleState {
@@ -23,6 +25,8 @@ impl ExampleState {
         Self {
             value: 0,
             running_miners: Vec::new(),
+            last_mined_block: 0,
+            block_commits: HashMap::new(),
         }
     }
 
@@ -42,6 +46,21 @@ impl ExampleState {
         self.running_miners.push(miner_seed);
         // Log the updated state.
         println!("Running miners: {:?}", self.running_miners);
+    }
+
+    pub fn add_block_commit(&mut self, height: u64, miner_seed: Vec<u8>) {
+        // Log the updated state.
+        println!(
+            "Block commit at height {} by miner {:?}",
+            height, miner_seed
+        );
+        let existing_commits = self.block_commits.entry(height).or_insert(Vec::new());
+        existing_commits.push(miner_seed);
+        println!(
+            "Block commiters for height {}: {:?}",
+            height,
+            self.block_commits.get(&height)
+        );
     }
 }
 
@@ -79,6 +98,47 @@ impl Command for StartMinerCommand {
 
     fn label(&self) -> &'static str {
         "START_MINER"
+    }
+}
+
+pub struct SubmitBlockCommitCommand {
+    miner_seed: Vec<u8>,
+}
+
+impl SubmitBlockCommitCommand {
+    pub fn new(miner_seed: Vec<u8>) -> Self {
+        // Check validity here. Prevent invalid data from being created.
+        Self { miner_seed }
+    }
+}
+
+impl Command for SubmitBlockCommitCommand {
+    fn check(&self, state: &ExampleState) -> bool {
+        // A miner can submit a block commit only if:
+        // 1. The miner is running.
+        // 2. The miner has not submitted a block commit at the same height.
+        state
+            .running_miners
+            .iter()
+            .any(|running| running == &self.miner_seed)
+            && !state
+                .block_commits
+                .get(&(state.last_mined_block + 1))
+                .map(|commits| commits.contains(&self.miner_seed))
+                .unwrap_or(false)
+    }
+
+    fn apply(&self, state: &mut ExampleState) {
+        println!(
+            "Submitting block commit at height {} by miner {:?}",
+            state.last_mined_block + 1,
+            self.miner_seed
+        );
+        state.add_block_commit(state.last_mined_block + 1, self.miner_seed.clone());
+    }
+
+    fn label(&self) -> &'static str {
+        "SUBMIT_BLOCK_COMMIT"
     }
 }
 
@@ -172,6 +232,8 @@ proptest! {
               Just(CommandWrapper::new(ShellProcCommand)),
               proptest::sample::select(&MINER_SEEDS)
               .prop_map(|seed| CommandWrapper::new(StartMinerCommand::new(seed.to_vec()))),
+              proptest::sample::select(&MINER_SEEDS)
+              .prop_map(|seed| CommandWrapper::new(SubmitBlockCommitCommand::new(seed.to_vec()))),
           ],
           1..10, // Change to something higher like 70.
       )
